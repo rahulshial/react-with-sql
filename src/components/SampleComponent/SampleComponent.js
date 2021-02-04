29b9b9ea8fa56fdded17974dd7d30c2b94d88ab4
Rahul Shial <rahul.shial@gmail.com>
2021-02-04 13:47:52 -0700

    React server with SQL

---
+++ src/components/SampleComponent/SampleComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function getUsers() {
+  fetch(`/api/users`)
+  .then((response) => response.json())
+  .then(users => console.log(users));
+}
+
+export default function SampleComponent() {
+  return (
+    <div>
+      <p>This is a sample component</p>
+      {getUsers()}
+    </div>
+  )
+};
